ecba6cdb2c5f2ff992038fa6bdc2a5ef495e9848
andrei-stoica26 <andreistoica@foxmail.com>
2025-07-21 18:22:30 +0800

    First commit

---
+++ R/composition.R
@@ -0,0 +1,58 @@
+#' @importFrom dplyr count
+#' @importFrom sgof BY
+#' @importFrom stats phyper setNames
+#'
+NULL
+
+#' Extract count information from Seurat column.
+#'
+#' This function extracts count information from Seurat column.
+#'
+#' @param seuratObj A Seurat object.
+#' @param column Column
+#'
+#' @return A frequency vector with the unique column values as names.
+#'
+countsVector <- function(seuratObj, column){
+    df <- dplyr::count(seuratObj[[]], {{column}})
+    v <- setNames(df[, 2], as.factor(df[, 1]))
+    return(v)
+}
+
+#' Find the differential representation of two Seurat columns
+#'
+#' This function find the differential representation of two Seurat columns.
+#'
+#' @param seuratObj A Seurat object.
+#' @param column1 First column.
+#' @param column2 Second column.
+#' @param doOverrep Whether to perform overrepresentation analysis. If
+#' \code{FALSE}, underrepresentation analysis will be performed.
+#' @param pvalThr p-value threshold.
+#'
+#' @return An overrepresentation or underrepresentation data frame.
+#'
+#' @export
+#'
+#'
+repAnalysis <- function(seuratObj, column1, column2, doOverrep=TRUE,
+                        pvalThr=0.05){
+    nCells <- ncol(seuratObj)
+    df <- dplyr::count(seuratObj[[]], {{column1}}, {{column2}})
+    colnames(df)[3] <- 'sharedCount'
+    v1 <- countsVector(seuratObj, {{column1}})
+    v2 <- countsVector(seuratObj, {{column2}})
+    newCols <- paste0(colnames(df)[c(1, 2)], 'Count')
+    df[[newCols[1]]] <- vapply(df[, 1], function(x) v1[x], integer(1))
+    df[[newCols[2]]] <- vapply(df[, 2], function(x) v2[x], integer(1))
+    df$expectedCount <- df[, 4] * df[, 5] / nCells
+    df$ratio <- df$sharedCount / df$expectedCount
+    df$pval <- mapply(function(q, m, n, k)
+        phyper(q, m, n, k, lower.tail=1 - doOverrep),
+        df[, 3] - doOverrep, df[, 4], nCells - df[, 4], df[, 5])
+    df <- df[order(df$pval, decreasing=TRUE), ]
+    df$pvalAdj <- BY(df$pval, pvalThr)$Adjusted.pvalues
+    df <- subset(df, pvalAdj < pvalThr)
+    return(df)
+}
+
---
+++ README.md
@@ -0,0 +1,20 @@
+# hammers
+`hammers` is an evolving collection of simple tools designed to address common 
+tasks in scRNA-seq data analysis. The included tools are intended to combine 
+wide applicability with ease of use.
+
+## Installation
+
+To install `hammers`, run the following R code:
+
+```
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+    
+BiocManager::install("andrei-stoica26/hammers")
+```
+## Functions implemented so far
+
+`repAnalysis`: Given two gene expression metadata categorical column, performs
+overrepresentation/underrepresentation analysis for all the pairs comprising a
+label from each category.
